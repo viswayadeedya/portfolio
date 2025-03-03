f0103dac2aa4c77a09caf8f4dbc2bc73cd7cd9bb
Viswa Yadeedya <viswayadeedya@gmail.com>
2025-03-03 05:17:00 -0500

    removed navigation

--- src/app/home/about/about.component.ts
+++ src/app/home/about/about.component.ts
@@ -8,23 +8,9 @@ import { ActivatedRoute } from '@angular/router';
   styleUrl: './about.component.scss',
 })
 export class AboutComponent implements AfterViewInit {
-  @ViewChild('about', { static: true }) about!: ElementRef;
-
   constructor(private route: ActivatedRoute) {}
 
   ngAfterViewInit() {
-    setTimeout(() => {
-      if (this.about) {
-        this.about.nativeElement.style.display = 'block';
-
-        this.about.nativeElement.classList.remove(
-          'opacity-0',
-          '-translate-x-full'
-        );
-        this.about.nativeElement.classList.add('opacity-100', 'translate-x-0');
-      }
-    }, 300);
-
     this.route.fragment.subscribe((fragment) => {
       if (fragment) {
         this.scrollToSection(fragment);
